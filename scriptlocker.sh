4ead215fef77d931006c14ed1dca0e3bda160f8c
Matt Harris <mjharris2407@gmail.com>
2019-04-10 22:02:44 -0400

    refactoring

--- scriptlocker.sh
+++ scriptlocker.sh
@@ -5,14 +5,14 @@
 #  m@ttHarri.is
 #  11/14/2017, 12:01:19 PM
 # =============================================================================
-# SCRIPTLOCKER - HANDLES YOUR EXECUTION FOR YOUR SCRIPTS. STORES THEM AND
+# "SCRIPT LOCKER" - HANDLES YOUR EXECUTION FOR YOUR SCRIPTS. STORES THEM AND
 # GETS THEM CALLS THEM FROM ANY PATH.
 # DYNAMIC SCRIPT LOADER AND MANAGER
 # HELPS YOU WRITE SMALL SCRIPTS AND CALL THEM FROM ANYWHERE
 # WITH PATHS AND ENV SET CORRECTLY
 
 # SET THIS IN THE EQUIVALNET .ENV TO PROVIDE A CONFIGURATION FILE OUTSDIE OF THE THE CODE
-if [ -z $SCRIPTLOCKER ]; then
+if [ -z $SCRIPTLOADER ]; then
   return
 fi
 
@@ -28,32 +28,62 @@ _scripts_info () {
     echo "  --script-edit [script]] - EDIT SCRIPT"
 }
 
+_script_list () {
+  SCRIPTS_PATH="$LIB0_ROOT/scripts"
+  ls $SCRIPTS_PATH
+}
 
 
 _script_loader () {
   # INCLDUE_SCRIPTS_IN_PATH=
-  # DEFAULTS TO FALSE, SINCE MANY OF THE SCRIPTS HERE ARE MEANT TO BE RUN ONE TIME
-  # OR DO SOEMTHING SPECIFIC ENOUGH THAT WE DO NOT WANT OT EXECUTE THEM ACCIDENTALLY. 
-  # The /bin folder is already added to the path. Scripts here are meant be stored outside the default
-  # path since they are accessed through a scpecial command. 
-  
-  # Default 
-  ENABLE_SCRIPTLOCKER=true
-  ENABLE_SCRIPTLOCKER_LOADER=true
-  if [ -z $ENABLE_SCRIPTLOCKER ]; then
-    echo "scriptloader is not enabled. Set ENABLE_SCRIPTLOCKER=true in your environment file"
+  # DEFAULTS TO FALSE, SINCE MANY OF THE SCRIPTS HERE ARE MEANT TO BE RUN ONE TIME OR DO SOEMTHING SPECIFIC ENOUGH THAT
+  # WE DO NOT WANT OT EXECUTE THEM ACCIDENTALLY. The bin/ folder is already added to the path. Scripts here are meant for
+  # more complicated operations.
+  ENABLE_LIB0SCRIPTS=true
+  ENABLE_LIB0SCRIPT_LOADER=true
+  if [ -z $ENABLE_LIB0SCRIPTS ]; then
+    echo "Commandctl scriptloader is not enabled. Set ENABLE_LIB0SCRIPTS=true in your environment file"
     return
   fi
 
   SCRIPT_NAME=${1-list}
   SCRIPT_ARGS=${2:-}
 
-  SCRIPTLOCKER_PATH="$LIB0_SHELLPATH/scripts"
-  SCRIPTLOCKER=false
+  LIB0_SCRIPTSPATH="$LIB0_ROOT/scripts"
+  LIB0_SCRIPT_VERBOSE=false
 
   if [ -z $SCRIPT_NAME ]; then
-    echo "sh $SCRIPTLOCKER_PATH/$SCRIPT_NAME $SCRIPT_ARGS"
-    sh $SCRIPTLOCKER_PATH/$SCRIPT_NAME $SCRIPT_ARGS
+    echo "sh $LIB0_SCRIPTSPATH/$SCRIPT_NAME $SCRIPT_ARGS"
+    sh $LIB0_SCRIPTSPATH/$SCRIPT_NAME $SCRIPT_ARGS
   fi
 
 }
+
+
+
+
+scriptlocker () {
+  ACTION=${1:-help}
+  echo "scriptlocker: $ACTION"
+
+  case $ACTION in
+     list)
+        _script_list
+        ;;
+
+     help)
+        _script_list
+        ;;
+
+    run)
+       SELECTION=${2:?The show command requires parameter}
+        _script_loader $SELECTION
+       ;;
+
+       *)
+        _script_list
+      ;;
+  esac
+}
+
+scriptlocker $@
